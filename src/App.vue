db517aed0a07efc7de60b10b5eb37c6723e27a54
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-03-19 17:19:37 -0400

    some clean up; reloading linux and retracing last few steps

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/vue-logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Generated Welcome</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
@@ -21,7 +21,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <style>
-@import '@/assets/base.scss';
+@import '@/assets/scss/base.scss';
 
 #app {
   max-width: 1280px;
