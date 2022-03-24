134d01799c9e9c6ba83ad7804df513564daaf988
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-03-24 19:23:13 -0400

    copy-pastad building 1, building 2, and profiles information from shannon's doc

--- src/components/ContentProfiles.vue
+++ src/components/ContentProfiles.vue
@@ -1,6 +1,41 @@
 <template>
 	<div>
-		content_profiles
+		Faculty and Staff
+M D Windburn
+Educator from 1881 to 1882
+
+Annie Ryers
+Educator from 1882 to 1883
+
+Phoebe Lulu Warbritton Brown
+1862 to 1942
+
+Educator from 1883 to 1884
+
+
+Lillian Elinor Britton
+1858 to 1923
+
+Educator from 1884 to 1887
+
+
+Thomas Jackson
+1801- 1891
+
+Building caretaker from 1884 to 1887
+
+
+E B Allen
+Dates unknown
+
+Educator of the upper elementary grades from 1886 to 1890
+	
+Jennie 
+
+Mannie Spillman
+
+Building Caretaker 1890- 1893
+
 	</div>
 </template>
 
